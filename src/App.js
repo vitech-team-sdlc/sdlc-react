0e36b5f7345e48257bcf69104c426235a7ccdfc4
Andriy Shvydchenko <andriy.shvydchenko@vitech.com.ua>
2020-12-09 15:36:06 +0200

    added React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Switch>
+            <Route path="/hello">
+              <p>Hello World!</p>
+              <p>
+                <a href="/" className="App-link">Back to index</a>
+              </p>
+            </Route>
+            <Route path="/">
+              <p>
+                <a href="/hello" className="App-link">Go to Hello World</a>
+              </p>
+            </Route>
+          </Switch>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+    </Router>
   );
 }
 
